b981d236fa2c1f6eba840cb06a77f28bd0aaeb19
Igor110717 <igorkotov110717@gmail.com>
2025-03-03 14:04:41 +0200

    решает 3,4 и 5 задание

--- script.js
+++ script.js
@@ -54,3 +54,144 @@
 // });
 
 // poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+
+// // Задание 3
+
+// class Storage {
+//     constructor (items) {
+//         this.items = items;
+//     }
+//     getItems() {
+//         return this.items;
+//     }
+//     addItem(item) {
+//         this.items.push(item);
+//     }
+//     removeItem(item) {
+//            for (let i = 0; i < this.items.length; i += 1) {
+//       if (item === this.items[i]) {
+//         this.items.splice(i, 1);
+//       }
+
+//         }
+//     }
+// }
+// const storage = new Storage([
+//     'Нанитоиды',
+//     'Пролонгер',
+//     'Железные жупи',
+//     'Антигравитатор',
+//   ]);
+  
+//   const items = storage.getItems();
+//   console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+  
+//   storage.addItem('Дроид');
+//   console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+  
+//   storage.removeItem('Пролонгер');
+//   console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+  
+
+// Задание 4
+
+// class StringBuilder {
+//     constructor(_value) {
+//         this._value = _value;
+//     }
+//     get value() {
+//         return this._value;
+//     }
+//     append(str) {
+//       this._value += str;
+//     }
+//     prepend(str) {
+//         this._value = str + this._value;
+//     }
+//     pad(str) {
+//            this._value = str + this._value + str;
+//     }
+// }
+
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
+
+// // Задание 5
+
+// class Car { 
+//     static getSpecs(car) {
+//     console.log(car.maxSpeed, car.speed, car.isOn, car.distance, car.price);
+//     }
+//     constructor ({price, maxSpeed, }) {
+//     this.speed = 0;
+//     this._price = price;
+//     this.maxSpeed = maxSpeed;
+//     this.isOn = false; 
+//     this.distance = 0; 
+//     }
+
+//     get price() {
+//         return this._price
+//     };
+   
+//     set price(newPrice) {
+//         this._price = newPrice;
+//     }
+    
+//     turnOn() {
+//         this.isOn = true;
+//     }
+    
+//     turnOff() {
+//         this.isOn = false
+//         this.speed = 0;
+//     }
+
+//     accelerate(value) {
+//         if (this.speed < this.maxSpeed) {
+//             this.speed += value;
+//         }
+//     }
+
+//     decelerate(value) {
+//         if (this.speed -value >= 0) {
+//             this.speed -= value;
+//         }
+//     }
+
+//     drive(hours) {
+
+//     if ( this.isOn === true) {
+//         this.distance +=  hours * this.speed
+//     }
+//     }
+// }
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
